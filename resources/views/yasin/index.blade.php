320503245987b7b6f6b32f3ce64eedd80855fc18
Fauzan Aldi <aldifauzan085@gmail.com>
2021-07-04 21:43:46 +0700

    fix testimoni

--- resources/views/yasin/index.blade.php
+++ resources/views/yasin/index.blade.php
@@ -227,6 +227,35 @@
                         
                     </div>
                 </div>
+                <div class="row wow fadeIn">
+
+                    @foreach($u as $key)
+          
+                        @foreach($key->testimoni as $key2)
+          
+                          <!--Grid column-->
+                          <div class="col-lg-4 col-md-12 mb-4" >
+                              @if(strpos($key2->file, 'mp4') !== false)
+                      
+                                  <video style="width: 100%; max-height: 500px;" controls="">
+                                      <source src="{{ asset('uploads/'.$key2->file) }}" type="video/mp4">
+                                  </video>
+          
+                              @else
+                              <a href="{{ asset('uploads/'.$key2->file) }}" data-lightbox="image-1" data-title="File Utama">
+                                  <img src="{{ asset('uploads/'.$key2->file) }}" class="img-fluid" alt="">
+                              </a>
+          
+                              @endif
+          
+                          </div>
+          
+                        @endforeach
+          
+                  @endforeach
+                  <!--Grid column-->
+          
+                </div>
             </div>
         </div>
       </div>
