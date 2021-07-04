ba3fdba30a0c1bec164d8b015cba00fc8d077d35
Fauzan Aldi <aldifauzan085@gmail.com>
2021-07-04 21:17:13 +0700

    fix testimoni

--- resources/views/yasin/index.blade.php
+++ resources/views/yasin/index.blade.php
@@ -203,19 +203,21 @@
                         @foreach($u as $key)
 
                             @foreach($key->testimoni as $key2)
-                            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 key{{ $key->id }}">
-                                
                                 @if(strpos($key2->file, 'mp4') !== false)
-                                <div class="item">
-                                    {{-- <video style="width: 100%; max-height: 700px; position: relative; " controls="">
-                                        <source src="{{ asset('uploads/'.$key2->file) }}" type="video/mp4">
-                                    </video> --}}
-                                </div>
+                                    <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 key{{ $key->id }}">
+                                        <div class="item">
+                                            <video style="width: 100%; max-height: 700px; position: relative; " controls="">
+                                                <source src="{{ asset('uploads/'.$key2->file) }}" type="video/mp4">
+                                            </video>
+                                        </div>
+                                    </div>
+                                
                                 @else
-                                <div class="item">
-                                    <a href="{{ asset('uploads/'.$key2->file) }}" data-lightbox="image-1" data-title="Our Projects"><img src="{{ asset('uploads/'.$key2->file) }}" alt=""></a>
-                                </div>
-
+                                    <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 key{{ $key->id }}">
+                                        <div class="item">
+                                            <a href="{{ asset('uploads/'.$key2->file) }}" data-lightbox="image-1" data-title="Our Projects"><img src="{{ asset('uploads/'.$key2->file) }}" alt=""></a>
+                                        </div>
+                                    </div>
                                 @endif
                             </div>
                             @endforeach
